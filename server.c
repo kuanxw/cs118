fcc87ca74b7dc2d1aed320a24fbacbe2aaa93b8d
kuanxw <kuanxw@g.ucla.edu>
2018-02-02 14:43:29 -0800

    Added input parsing, 404 error and spacebar fix

--- server.c
+++ server.c
@@ -1,8 +1,3 @@
-/* A simple server in the internet domain using TCP
-   The port number is passed as an argument
-   This version runs forever, forking off a separate
-   process for each connection
-*/
 #include <stdio.h>
 #include <fcntl.h>
 #include <sys/types.h>   // definitions of a number of data types used in socket.h and netinet/in.h
@@ -14,6 +9,45 @@
 #include <sys/stat.h>
 #include <signal.h>  /* signal name macros, and the kill() prototype */
 
+char* content_type;
+
+int parse_file_req(char* buf){
+    char filename[2048];
+    bzero(filename,2048);
+    //Isolate substring that is the filename
+    char* fn_start = strstr(buf,"GET /") + 5;
+    char* fn_end = strstr(buf," HTTP/1.1\r\n");
+    size_t fn_size = (fn_end - fn_start)/sizeof(char);
+    if(fn_size < 0 || fn_size > 2048){
+        printf("Invalid file name size: %d\n",fn_size);
+        exit(0);
+    }
+    //Special case: No/Default file requested index.html 
+    if (fn_size == 0) {
+        strcat(filename, "index.html");
+        fn_size = 10;
+    }
+    else memcpy(filename,fn_start,fn_size);
+    
+    //Replace "%20" strings with spaces
+    char* marker;
+    while((marker = strstr(filename,"%20")) != NULL){
+        *marker = ' ';
+        strncpy(marker+1,marker+3,fn_size-((marker+3)-filename)/sizeof(char));
+        fn_size = fn_size - 2;
+        memset(filename+fn_size*sizeof(char),0,2);
+    }
+    //printf("File name: %s\n",filename);
+    //Extract file type
+    marker = strrchr(filename,'.');
+    if(marker == NULL || strcmp(marker+1,"html")==0) content_type = "text/html";
+    else if(strcmp(marker+1,"jpg")==0) content_type = "image/jpeg";
+    else if(strcmp(marker+1,"gif")==0) content_type = "image/gif";
+    else content_type = "application/octet-stream";
+    //Import file
+    int fd = open(filename, O_RDONLY);
+    return fd;
+}
 
 void error(char *msg)
 {
@@ -24,7 +58,6 @@ void error(char *msg)
 int main(int argc, char *argv[])
 {
     int sockfd, newsockfd, portno;
-    int process_id;
     socklen_t clilen;
     struct sockaddr_in serv_addr, cli_addr;
 
@@ -59,30 +92,38 @@ int main(int argc, char *argv[])
 
     // SERVER request and response
     int n;
-    char buffer[2048];
+    char in_buffer[2048];
     char fn[1024];
-
-    memset(buffer, 0, 2048);  // reset memory
-    memset(fn, 0, 1024); // reset file name
-
-    //read client's message
-    n = read(newsockfd, buffer, 2047);
-    if (n < 0) error("ERROR reading from socket");
-    printf("Here is the message: %s\n", buffer);
-    
-
-    int file_descriptor; 
-    file_descriptor = open("index.html", O_RDONLY);
-
+    char header[2048];
     char wrbuf[512];
     bzero(wrbuf, sizeof(wrbuf));
 
-    char* header = "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\n";
+    memset(in_buffer, 0, 2048);  // reset memory
+    memset(fn, 0, 1024); // reset file name
+    memset(header,0,2048); //reset header memory
+
+    //read client's message
+    n = read(newsockfd, in_buffer, 2047);
+    if (n < 0) error("ERROR reading from socket");
+    printf("Here is the message: %s\n", in_buffer);
+    
+    //Extract file name, get its file descriptor, and modify content_type
+    int fd = parse_file_req(in_buffer);
+    if(fd < 0){
+        strcat(header,"HTTP/1.1 404 Not Found\r\nContent-Type: text/html\r\n\r\n");
+        if((fd = open("404.html",O_RDONLY)) < 0){
+            printf("404 File is also not found\n");
+            return 1;
+        }
+    } else {
+        strcat(header,"HTTP/1.1 200 OK\r\nContent-Type: ");
+        strcat(header,content_type);
+        strcat(header,"\r\n\r\n");
+    }
     write(newsockfd, header, strlen(header));
 
     while (1) {
-
-        int num_chars_read = read(file_descriptor, wrbuf, 512);
+        int num_chars_read = read(fd, wrbuf, 512);
 
         //reply to client
         n = write(newsockfd, wrbuf, strlen(wrbuf));
